613db304e918d1e81f9ff34ccf2ebd436cb6a5bf
jtimberman <joshua@opscode.com>
2013-06-03 22:55:14 -0600

    release v0.1.1

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email 'joshua@opscode.com'
 license          'Apache 2.0'
 description      'Manages racoon configuration for OS X'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 supports         'mac_os_x'
